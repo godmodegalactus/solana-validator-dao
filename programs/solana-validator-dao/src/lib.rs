c7072c546d3999c4ed7c702e36db85b133573997
godmode galactus <godmodegalactus@gmail.com>
2022-08-22 15:33:47 +0200

    changing back the progrom id for the contract

--- programs/solana-validator-dao/src/lib.rs
+++ programs/solana-validator-dao/src/lib.rs
@@ -9,7 +9,7 @@ mod processors;
 mod errors;
 mod states;
 
-declare_id!("BC9n2UZZP4vebGeHuMAAnSzeRA5rv3hCoPFqsybLLpQv");
+declare_id!("AwyKDr1Z5BfdvK3jX1UWopyjsJSV5cq4cuJpoYLofyEn");
 
 const GOVERNANCE_PROGRAM_ID: Pubkey = pubkey!("GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw");
 
@@ -22,45 +22,45 @@ pub mod solana_validator_dao {
         processors::process_stake::process(ctx, seed, lamports)
     }
 
-    pub fn register_validator_provider(
-        ctx: Context<RegisterValidatorServiceProvider>,
-        services: u64,
-        name: String,
-        description: String,
-    ) -> Result<()> {
-        processors::register_validator_provider::process(ctx, services, name, description)
-    }
+    // pub fn register_validator_provider(
+    //     ctx: Context<RegisterValidatorServiceProvider>,
+    //     services: u64,
+    //     name: String,
+    //     description: String,
+    // ) -> Result<()> {
+    //     processors::register_validator_provider::process(ctx, services, name, description)
+    // }
 
-    pub fn add_registered_provider_to_governance(
-        ctx: Context<AddRegisteredProviderToGovernance>,
-    ) -> Result<()> {
-        processors::add_registered_provider_to_governance::process(ctx)
-    }
+    // pub fn add_registered_provider_to_governance(
+    //     ctx: Context<AddRegisteredProviderToGovernance>,
+    // ) -> Result<()> {
+    //     processors::add_registered_provider_to_governance::process(ctx)
+    // }
 
-    pub fn create_governance_contract(
-        ctx: Context<CreateGovernanceContract>,
-        _contract_seed: u64,
-        services: u64,
-        contract_start_unix_timestamp: u64,
-        contract_end_unix_timestamp: u64,
-        initial_amount: u64,
-        recurring_amount: u64,
-        periodicity: u8,
-        number_of_periods: u32,
-    ) -> Result<()> {
-        processors::create_governance_contract::process(
-            ctx,
-            services,
-            contract_start_unix_timestamp,
-            contract_end_unix_timestamp,
-            initial_amount,
-            recurring_amount,
-            periodicity.into(),
-            number_of_periods,
-        )
-    }
+    // pub fn create_governance_contract(
+    //     ctx: Context<CreateGovernanceContract>,
+    //     _contract_seed: u64,
+    //     services: u64,
+    //     contract_start_unix_timestamp: u64,
+    //     contract_end_unix_timestamp: u64,
+    //     initial_amount: u64,
+    //     recurring_amount: u64,
+    //     periodicity: u8,
+    //     number_of_periods: u32,
+    // ) -> Result<()> {
+    //     processors::create_governance_contract::process(
+    //         ctx,
+    //         services,
+    //         contract_start_unix_timestamp,
+    //         contract_end_unix_timestamp,
+    //         initial_amount,
+    //         recurring_amount,
+    //         periodicity.into(),
+    //         number_of_periods,
+    //     )
+    // }
 
-    pub fn execute_governance_contract(ctx: Context<ExecuteContract>) -> Result<()> {
-        processors::execute_contract::process(ctx)
-    }
+    // pub fn execute_governance_contract(ctx: Context<ExecuteContract>) -> Result<()> {
+    //     processors::execute_contract::process(ctx)
+    // }
 }
